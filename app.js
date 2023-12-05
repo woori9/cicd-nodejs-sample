606bdbc9448ee114e5eb670c035ba1b976b993f0
hiondal <hiondal@gmail.com>
2023-12-05 13:26:32 -0600

    test webhook

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-    res.send('Hello World!')
+    res.send('Hello Jenkins!')
 });
 
 const port = process.env.PORT || 5000;
